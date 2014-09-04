627b17c8f29a7384d0042af89975be471de7d985
Anthony C <broac@live.ca>
2014-09-04 16:11:27 -0700

    V1.0 - Generator, GUI and logic

---
+++ SudokuSolverGenerator/SudokuSolverGenerator.Shared/SudokuDataSource/SudokuPuzzle.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Sudoku
+{
+    
+    public class SudokuPuzzle
+    {
+        private List<List<int>> _cells = new List<List<int>>();
+        public List<List<int>> Cells { get { return _cells;} set {_cells = value;} }
+
+        private List<List<List<int>>> _solvedCells = new List<List<List<int>>>();
+        public List<List<List<int>>> SolvedCells { get { return _solvedCells; } set { _solvedCells = value; } }
+
+        public int Length { get; set; }
+        public int BoxSize { get { return (int)Math.Sqrt(Length); } }
+        
+
+        public SudokuPuzzle(int length)
+        {
+            this.Length = length;
+            
+        }
+
+        private bool CheckIfSolved(List<List<int>> cells)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.Count() != 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+        private void SolveSudoku(int numSolutions)
+        {
+            var workingCells = new List<List<int>>();
+            CopyCells(workingCells, Cells);    //Keep the starting puzzle and working solution separate
+
+            EliminateCandidates(workingCells, numSolutions);
+        }
+
+
+        private void EliminateCandidates(List<List<int>> cells, int numSolutions)
+        {
+            for (int cell = 0; cell < Length * Length; cell++)
+            {
+                if (cells[cell].Count > 1)
+                {
+                    if (FindCandidates(cells, cell) == false)
+                        return;
+                }
+            }
+
+            if (!(CheckIfSolved(cells)))
+                TestCandidate(cells, numSolutions);     //If puzzle not solved yet, test candidates
+            else
+            {
+                Debug.WriteLine("Solution Found");
+                SolvedCells.Add(cells);
+            }
+        }
+
+        private void TestCandidate(List<List<int>> cells, int numSolutions)        //Tests every allowed path in case of multiple solutions
+        {
+            int minCandidates = cells.Where(cell => cell.Count > 1).Min(cands => cands.Count);  //Find the lowest amount of candidates of all cells
+            int minCell = cells.FindIndex(cell => cell.Count == minCandidates);     //Find a cell that has that lowest number of candidates
+
+            foreach (int candidate in cells[minCell])
+            {
+                Debug.WriteLine("Testing Candidate");
+                var testCells = new List<List<int>>();
+                CopyCells(testCells, cells);         //Save cells before testing           
+
+                testCells[minCell] = new List<int> { candidate };
+                if (!(UpdateCandidates(testCells, minCell)))        //If this candidate causes contradiction, move to next one
+                    continue;
+
+                EliminateCandidates(testCells, numSolutions);     //Have to assign testCells because successive testCells may get held (after making a copy from which the program proceeds with instead) when reversing after solving
+
+                if (SolvedCells.Count() == numSolutions)
+                    return;
+            }
+
+            if (!(CheckIfSolved(cells)))        //If at end of candidates list and not solved, candidate of previous stack frame is contradictory
+                return;             //Using checkIfSolved since if found 1 solution, still want to check specific cells for other possible solutions 
+        }
+
+        public void CopyCells(List<List<int>> testCells, List<List<int>> Cells)
+        {
+            testCells.Clear();
+
+            for (int i = 0; i < Length * Length; i++)     //Deep copy to create seperate reference 
+            {
+                testCells.Add(new List<int>());
+
+                foreach (int cand in Cells[i])
+                {
+                    testCells[i].Add(cand);
+                }
+            }
+        }
+
+        private List<int> FindPeers(int cell)  //For a cell, find its related row, column and box peers. (Values will never change throughout)
+        {
+            var peers = new List<int>();
+
+            for (int peerIndex = 0; peerIndex < Length * Length; peerIndex++)
+            {
+                if (peerIndex / Length == cell / Length  //If in same row, add to peers
+                    | peerIndex % Length == cell % Length  //If in same column, add to peers
+                    | (peerIndex / Length / BoxSize == cell / Length / BoxSize && peerIndex % Length / BoxSize == cell % Length / BoxSize) //If in same box, add to peers
+                    && peerIndex != cell)  //Can't be same value for peer and cell
+                    peers.Add(peerIndex);
+            }
+
+            return peers;
+        }
+
+        private bool FindCandidates(List<List<int>> cells, int cell)  //Find the possible allowed values for a cell, judging by its peers
+        {
+            var peers = FindPeers(cell);
+
+            foreach (var peer in peers)
+            {
+                if (cells[peer].Count == 1 && cells[cell].Contains(cells[peer][0]))
+                {
+                    var cellCount = cells[cell].Count;
+                    cells[cell].Remove(cells[peer][0]);
+
+                    if (cellCount == 2)
+                    {
+                        if (!(UpdateCandidates(cells, cell)))
+                            return false;
+
+                        break;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool UpdateCandidates(List<List<int>> cells, int cell)      //If a cell is solved through FindCandidates, update its peers candidates, successively
+        {
+            var peers = FindPeers(cell);
+
+            foreach (var peer in peers)
+            {
+                if (cells[peer].Contains(cells[cell][0]))
+                {
+                    var peerLength = cells[peer].Count();
+
+                    if (peerLength == 1)            //Contradiction -- false solution thus far
+                        return false;
+
+                    cells[peer].Remove(cells[cell][0]);
+
+                    if (peerLength == 2)
+                    {
+                        if (!(UpdateCandidates(cells, peer)))
+                            return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+
+
+        public void GenerateSudoku(int numStartingCells)
+        {
+            var rand = new Random();        //Don't put rand inside AssignRandoCandidate since each will create new random objects in quick succession with the same seed - use 1 random object only
+            var eliminatedCells = new List<int>();
+
+            do
+            {
+                Debug.WriteLine("Trying new puzzle");
+                Cells = Enumerable.Repeat(new List<int>(Enumerable.Range(1, 9)), Length * Length).ToList();
+                SolvedCells.Clear();
+                eliminatedCells.Clear();
+
+                for (int randCell = 0; randCell < numStartingCells; randCell++)     //Iterate and add starting cells to sudoku puzzle
+                {
+                    if (CheckIfSolved(Cells))
+                        break;
+
+                    eliminatedCells.AddRange(AssignRandomCandidate(rand, Cells));      //Assigns the rand cand and then adds the eliminated cells as a result of this to the list
+
+                    if (eliminatedCells.Contains(-1))
+                        break;
+
+                }
+
+                if (eliminatedCells.Contains(-1))
+                    continue;
+
+                SolveSudoku(2);
+
+            } while (SolvedCells.Count() != 1);
+
+            Debug.WriteLine("Eliminated: {0}", eliminatedCells.Count());
+
+            foreach (int c in eliminatedCells)      //Get rid of the excess solved cells from generatedSudoku, for the final puzzle to the user
+            {
+                if (Cells.Where(d => d.Count() == 1).Count() == numStartingCells)
+                    break;
+
+                Cells[c] = new List<int>(Enumerable.Range(1, Length));
+            }
+
+        }
+
+        private List<int> AssignRandomCandidate(Random rand, List<List<int>> cells)
+        {
+
+            int cell;
+            var assignedCells = new List<List<int>>();      //The cells prior to assigning random candidate and possible peers being solved due to this
+            var eliminatedCells = new List<int>();
+
+            CopyCells(assignedCells, cells);
+
+            do
+            {
+                cell = rand.Next(Length * Length);   //Repeat until a random cell with more than 1 value is found
+
+            } while (cells[cell].Count() == 1);
+
+            var candidate = cells[cell][rand.Next(cells[cell].Count())];    //Finds a random candidate of the random cell and stores it
+
+            assignedCells[cell] = new List<int> { candidate };
+
+            if (!(UpdateCandidates(assignedCells, cell)))           //If assignment leads to contradiction, restart entire sudoku grid
+            {
+                eliminatedCells.Add(-1);
+                return eliminatedCells;
+            }
+
+            for (int c = 0; c < Length * Length; c++)        //Find the eliminated cells due to random candidate assignment so that they are not included in the puzzle for the user later
+            {
+                if (assignedCells[c].Count == 1 && cells[c].Count > 1 && c != cell)
+                    eliminatedCells.Add(c);
+            }
+
+            CopyCells(cells, assignedCells);
+            return eliminatedCells;
+        }
+
+        public void PrintSudoku(List<List<int>> Cells)
+        {
+            for (int w = 0; w < Length * Length; w++)
+            {
+                if (w % Length == 0)
+                    Debug.WriteLine("");
+                if (Cells[w].Count() == 1)
+                    Debug.WriteLine("{0} ", Cells[w][0]);
+                else if (Cells[w].Count() == 0)
+                    Debug.WriteLine(". ");
+                else
+                    Debug.WriteLine("0 ");
+            }
+
+            Debug.WriteLine("");
+        }
+
+        
+
+    }
+}
---
+++ SudokuSolverGenerator/SudokuSolverGenerator.WindowsPhone/SudokuMain.xaml.cs
@@ -0,0 +1,278 @@
+﻿using Sudoku;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SudokuSolverGenerator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    
+    public sealed partial class SudokuMain : Page
+    {
+        private const string VALUESFILE = "values.json";
+        private int _selectedIndex = -1;
+        private Random rand = new Random();
+        private List<Border> _borders;
+        private List<TextBlock> _values;
+        private SudokuPuzzle puzzle;
+
+        
+
+        public SudokuMain()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            Create_Cells();
+            Create_Boxes();
+            try
+            {
+                Create_Sudoku((int)e.Parameter);
+            }
+            catch
+            {
+                Create_Sudoku(30);
+            }
+        }
+
+        private void Create_Sudoku(int numCells)
+        {
+            puzzle = new SudokuPuzzle(9);
+            puzzle.GenerateSudoku(numCells);
+
+            for (var startingCell = 0; startingCell < 81; startingCell++ )
+            {
+                if (puzzle.Cells[startingCell].Count == 1)
+                {
+                    _borders[startingCell].Background.ClearValue(SolidColorBrush.ColorProperty);
+                    _values[startingCell].Text = puzzle.Cells[startingCell][0].ToString();
+                    _values[startingCell].Foreground = new SolidColorBrush(Colors.DodgerBlue);
+                }
+
+            }
+
+        }
+        private void Create_Cells()
+        {
+            _values = new List<TextBlock>();
+            _borders = new List<Border>();
+
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+
+                    var cell = new TextBlock ();
+                    Grid.SetRow(cell, row);
+                    Grid.SetColumn(cell, col);
+
+                    var border = new Border { BorderBrush = new SolidColorBrush(Colors.White), BorderThickness = new Thickness(0.5), Background = new SolidColorBrush(Colors.Black) };
+
+                    border.Tapped += Cell_Tapped;
+                    Grid.SetRow(border, row);
+                    Grid.SetColumn(border, col);
+
+                    var viewBox = new Viewbox();
+                    Grid.SetRow(viewBox, row);
+                    Grid.SetColumn(viewBox, col);
+
+                    SudokuGrid.Children.Add(viewBox);
+                    SudokuGrid.Children.Add(border);
+                    viewBox.Child = cell;
+                }
+            }
+
+            _values = SudokuGrid.Children.Where(c => c is Viewbox).Select(c => (TextBlock)((Viewbox)c).Child).ToList();
+            _borders = SudokuGrid.Children.Where(c => c is Border).Select(c => (Border)c).ToList();
+        }
+
+        private void Create_Boxes()
+        {
+            for (var row = 0; row < 3; row++)
+            {
+                for (var col = 0; col < 3; col++)
+                {
+                    var box = new Border();
+                    box.BorderThickness = new Thickness(2.5);
+                    box.BorderBrush = new SolidColorBrush(Colors.White);
+                    Grid.SetRow(box, row * 3);
+                    Grid.SetColumn(box, col * 3);
+                    Grid.SetRowSpan(box, 3);
+                    Grid.SetColumnSpan(box, 3);
+                    SudokuGrid.Children.Add(box);
+                }
+
+            }
+        }
+
+        private void Cell_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var border = (Border)sender;        
+            var newIndex = Grid.GetRow(border) * 9 + Grid.GetColumn(border); 
+
+            if (puzzle.Cells[newIndex].Count == 1)      //Don't making starting cells clickable
+                return;
+
+            //Previous selection algorithim
+            if (_selectedIndex != -1 )          
+            {
+                if (!(_values[_selectedIndex].Text.Any()))
+                    _borders[_selectedIndex].Background.ClearValue(SolidColorBrush.ColorProperty);
+
+                else if ((_values[_selectedIndex].Text.Count() == 1))
+                    _values[_selectedIndex].Foreground = new SolidColorBrush(Colors.White);
+                else
+                    _values[_selectedIndex].Foreground = new SolidColorBrush(Colors.Gold);
+            }
+
+            //Current selection algorithim
+            _selectedIndex = newIndex;     
+
+            if (_values[_selectedIndex].Text.Any())        
+                _values[_selectedIndex].Foreground = new SolidColorBrush(Colors.ForestGreen);
+            else
+                _borders[_selectedIndex].Background = new SolidColorBrush(Colors.ForestGreen);
+
+        }
+
+        private void Value_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (_selectedIndex == -1)     
+                return;
+
+            var valueChoice = (TextBlock)sender;
+
+            _borders[_selectedIndex].Background.ClearValue(SolidColorBrush.ColorProperty);      //Remove highlight background if any
+
+            if (!(_values[_selectedIndex].Text.Contains(valueChoice.Text)))         //Algorithm to manage values in a cell
+            {
+                var cellValues = _values[_selectedIndex].Text + valueChoice.Text;
+                var charValues = cellValues.ToCharArray();
+                Array.Sort(charValues);
+                _values[_selectedIndex].Text = "";
+
+                foreach (var a in charValues)
+                {
+                    _values[_selectedIndex].Text += a;
+                }
+            }
+
+            if (_values[_selectedIndex].Text.Length == 1)
+                _values[_selectedIndex].Foreground = new SolidColorBrush(Colors.ForestGreen);       //Highlight foreground if assigned, otherwise background will cover number
+            else
+                _values[_selectedIndex].Foreground = new SolidColorBrush(Colors.Gold);          //To indicate multiple candidates 
+
+            if (_values.Where(c => c.Text.Any()).Count() == 81)         //Check if placement solves puzzle        
+            {
+                if (_values.Select(c => c.Text).SequenceEqual(puzzle.SolvedCells[0].Select(c => c[0].ToString())))
+                {
+                    VictoryMP3.Play();
+                }
+            }
+
+        }
+
+        private void Clear_Cell(object sender, TappedRoutedEventArgs e)
+        {
+
+            if (_selectedIndex == -1)
+                return;
+
+            if (_values[_selectedIndex].Text.Any())
+            {
+                _values[_selectedIndex].Text = "";
+                _borders[_selectedIndex].Background = new SolidColorBrush(Colors.ForestGreen);
+            }
+                
+                
+        }
+
+        private void Hint_Click(object sender, RoutedEventArgs e)
+        {
+            int cell;
+
+            do
+            {
+                cell = rand.Next(puzzle.Length * puzzle.Length);   //Repeat until a random cell with more than 1 value is found
+
+            } while (_values[cell].Text == puzzle.SolvedCells[0][cell][0].ToString());
+
+            _borders[cell].Background.ClearValue(SolidColorBrush.ColorProperty);
+            _values[cell].Foreground = new SolidColorBrush(Colors.White);
+            _values[cell].Text = puzzle.SolvedCells[0][cell][0].ToString();
+        }
+
+        private void Generate_Easy(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SudokuMain), 35);
+        }
+
+        //private async Task SerializeValuesAsync()
+        //{
+        //    var serializer = new DataContractJsonSerializer(typeof(SudokuPuzzle));
+
+        //    using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync(VALUESFILE, CreationCollisionOption.ReplaceExisting))
+        //    {
+        //        serializer.WriteObject(stream, puzzle);
+        //    }
+
+        //}
+
+        //private async Task<bool> DeserializeValuesAsync()
+        //{
+        //    var serializer = new DataContractJsonSerializer(typeof(List<string>));
+
+        //    try
+        //    {
+        //        using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync(VALUESFILE))
+        //        {
+        //            puzzle = (SudokuPuzzle)serializer.ReadObject(stream);
+        //        }
+
+        //        return true;
+        //    }
+        //    catch       //If no saved file, get ready to generate new pzuzle
+        //    {
+        //        return false;
+        //    }
+        //}
+  
+
+    }
+}
